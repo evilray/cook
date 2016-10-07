67566065e031eb1437c4ad58959319b0e8943cb6
Ankh <ankh-bast-maat@yandex.ru>
2016-10-07 18:47:09 +0200

    добавили форму в шаблон, установили js-загрузщика

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
 	        new FOS\RestBundle\FOSRestBundle(),
 	        new FOS\UserBundle\FOSUserBundle(),
 	        new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Oneup\UploaderBundle\OneupUploaderBundle(),
 
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
